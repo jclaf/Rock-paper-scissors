80ed4d0429fd79f6a530933fc0d0865b7159cd84
Jean-Christophe LAFLEUR <fabrice.972.20@gmail.com>
2025-06-20 14:27:36 +0000

    finish

--- RPS.py
+++ RPS.py
@@ -1,48 +1,79 @@
+
+
+
 # To defeat all four opponents, your program may need
 # to have multiple strategies
 # that change depending on the plays of the opponent.
-
-## Randomly
-
-
-def random_strat():
-    return random.choice(["R", "P", "S"])
-
-
-## Anti-repetition - Quincy
-
-
-def anti_repetition_strat(opponent_history):
-    pass
-
-
-## frequency Analysis - Mrugesh
-
-
-def frequency_strat(opponent_history):
-    pass
-
-
-## Markov chain Models - Abbey
-
-
-def markov_chain_strat(opponent_history):
-    pass
-
-
-## pattern - Kris
-def pattern_strat(opponent_history):
-    pass
 
 
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
 
+# def player(prev_play, opponent_history=[]):
+#     opponent_history.append(prev_play)
+
+#     guess = "R"
+#     if len(opponent_history) > 2:
+#         guess = opponent_history[-2]
+
+#     return guess
+
+def beat(prev_play):
+    return {"R": "P", "P": "S", "S": "R"}[prev_play]
+
 def player(prev_play, opponent_history=[]):
+
+    # player.pattern_tracker is a dictionary 
+    # that tracks how often specific sequences of the opponent’s moves occur
+    if not hasattr(player, 'pattern_tracker'):
+        player.pattern_tracker = {}
+        
+    if not prev_play:
+        prev_play = 'R'
+
     opponent_history.append(prev_play)
+    pattern_tracker = player.pattern_tracker
+    
+    # n is the number of previous moves used for pattern matching
+    # n = 4 is the best number of n but 5 is also good
+    n = 4
+    if len(opponent_history) > n:
+        last_seq = "".join(opponent_history[-(n+1):])
+        pattern_tracker[last_seq] = pattern_tracker.get(last_seq, 0) + 1
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+        potentials = [
+            "".join([*opponent_history[-n:], v]) 
+            for v in 'RPS'
+        ]
 
-    return guess
+        sub = {
+            k: pattern_tracker[k]
+            for k in potentials if k in pattern_tracker
+        }
+
+        pred = max(sub, key=sub.get)[-1:] if sub else 'P'
+    else:
+        pred = 'P'
+
+    return beat(pred)
+
+
+# Search best number of n  
+def make_player(n):
+    def player(prev_play, opponent_history=[]):
+        if not hasattr(player, 'pattern_tracker'):
+            player.pattern_tracker = {}
+        if not prev_play:
+            prev_play = 'R'
+        opponent_history.append(prev_play)
+        pt = player.pattern_tracker
+        if len(opponent_history) > n:
+            last_seq = ''.join(opponent_history[-(n+1):])
+            pt[last_seq] = pt.get(last_seq, 0) + 1
+            potentials = [''.join(opponent_history[-n:] + [v]) for v in 'RPS']
+            sub = {k: pt[k] for k in potentials if k in pt}
+            pred = max(sub, key=sub.get)[-1] if sub else 'P'
+        else:
+            pred = 'P'
+        return beat(pred)
+    return player
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This entrypoint file to be used in development. Start by reading README.md
 from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
-from RPS import player
+from RPS import player, make_player
 from unittest import main
 
 play(player, quincy, 1000)
@@ -14,7 +14,12 @@ play(player, mrugesh, 1000)
 # Uncomment line below to play against a bot that plays randomly:
 # play(human, random_player, 1000)
 
-
+# for n in range(2, 9):
+#   player = make_player(n)
+#   win_rates = []
+#   for bot in [quincy, abbey, kris, mrugesh]:
+#       win_rates.append(play(player, bot, 1000))
+#   print(f"n={n}: Quincy={win_rates[0]:.1f}%, Abbey={win_rates[1]:.1f}%, Kris={win_rates[2]:.1f}%, Mrugesh={win_rates[3]:.1f}%")
 
 # Uncomment line below to run unit tests automatically
-# main(module='test_module', exit=False)
+main(module='test_module', exit=False)
